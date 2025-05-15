86ec4dfa0c42cc2c96fb0e70626b42189b4cc4f3
patrickromaoext <patrick.romao.ext@asapcred.com.br>
2025-05-15 09:34:42 -0300

    clone_graph

---
+++ 1_graph/clone_graph.py
@@ -0,0 +1,25 @@
+
+# 0. Clone Graph: https://leetcode.com/problems/clone-graph/description/
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors: list[Node] = neighbors if neighbors is not None else []
+    
+class Solution:
+    def cloneGraph(self, node: Node) -> Node:
+        if not node:
+            return None
+
+        visited = {}
+
+        def dfs(n):
+            if n in visited:
+                return visited[n]
+            
+            clone = Node(n.val)
+            visited[n] = clone
+            for neighbor in n.neighbors:
+                clone.neighbors.append(dfs(neighbor))
+            return clone
+
+        return dfs(node)
